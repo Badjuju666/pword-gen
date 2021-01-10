371cf2f3f1967d967140d8bafda15be4a8a204f9
Badjuju666 <juliangraves@Julians-MBP.attlocal.net>
2021-01-09 20:46:06 -0800

    next up four loops

--- script.js
+++ script.js
@@ -1,21 +1,54 @@
 // Assignment code here
-let generateBtn = document.querySelector("#generate") 
-let lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-let uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-let specialChar = [" ", "!", "#", "$", "%", "^", "&", "+", "*", "-" ];
-let numbers = [0,1,2,3,4,5,6,7,8,9]
+// let lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+// let uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+// let specialChar = [" ", "!", "#", "$", "%", "^", "&", "+", "*", "-"];
+// let numbers = [0,1,2,3,4,5,6,7,8,9];
 // Get references to the #generate element
+
 var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
 function writePassword() {
+  var passwordLengthPrompt = window.prompt("What length of Characters do you need?");
 
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+if ( 
+  passwordLengthPrompt  === null || 
+  passwordLengthPrompt < 8 ||
+  passwordLengthPrompt > 128
+  ) {
+    window.alert(
+      "password must be over 8 characters but no more than 128!"
+    );
 
-  passwordText.value = password;
+} else {
+let lowercase = window.confirm("Do you want lower case letters in your password?")
+let uppercase =  window.confirm("Do you want upper case letters in your password?")
+let specialChar = window.confirm("Do you want special characters in your password?")
+let numbers =  window.confirm("Do you want numbers in your password?")
 
+
+var password = generatePassword ( lowercase, uppercase, specialChar, numbers);
+var passwordText = document.querySelector("#password");
+passwordText.value = password;
+} 
+{
+  function generatePassword (passwordlength, uppercase, lowercase, specialChar, numbers) {
+    var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+    var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    var numbers = [0,1,2,3,4,5,6,7,8,9];
+    var specialChar = [" ", "!", "#", "$", "%", "^", "&", "+", "*", "-"];
+  let password = "";
+  }
 }
+// 
+ // add in four loops
+// Write password to the #password input
 
-// Add event listener to generate button cccc
+// function writePassword() {
+
+//   var password = generatePassword();
+
+//   passwordText.value = password;
+
+// }
+// Add event listener to generate button
+};
 generateBtn.addEventListener("click", writePassword);
